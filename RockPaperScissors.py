aeefaac31ce5cff92295e25bd5e48c677e85ad62
SierraTran <sierra.tran@mail.com>
2023-07-24 10:50:14 -0400

    Wrote code to simulate Rock, Paper, Scissors

---
+++ RockPaperScissors.py
@@ -0,0 +1,37 @@
+import random
+
+"""
+This program simulates the "Rock, Paper, Scissors" game. 
+"""
+
+def play():
+    # The user is prompted for a choice, and the computer randomly chooses.
+    user = input("Pick one... \n'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    # This shows the user's choice and the computer's choice
+    print("You: "+user)
+    print("Computer: "+computer)
+
+    if user == computer:
+        return '\nIt\'s a tie'
+    
+    if is_win(user, computer):
+        return '\nYou won!'
+    
+    return '\nYou lost!'
+    
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+
+# This variable keeps the game loop going
+keep_playing = "y" 
+
+# This is the gam loop
+while(keep_playing == "y"):
+    print(play())
+    keep_playing = input("\nWould you like to keep playing? 'y' for yes, 'n' for no\n")
+
+print("Goodbye!")
