d1ae7ddbda5d9db1106900db8b8fb1342f4fecd0
SierraTran <sierra.tran@mail.com>
2023-08-04 19:14:34 -0400

    Created is_valid_input() and added more comments

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -9,9 +9,18 @@ def play():
     user = input("Pick one... \n'r' for rock, 'p' for paper, 's' for scissors\n").lower()
     computer = random.choice(['r', 'p', 's'])
 
+    # This variable uses the is_valid_choice function to make sure the user input can be used for the game
+    valid_choice = is_valid_choice(user)
+    
+    # If the user inut is not valid, this loop will start and run until valid input is received from the user
+    while(valid_choice == False):
+        print(user + " is not a valid choice. Please try again.")
+        user = input("Pick one... \n'r' for rock, 'p' for paper, 's' for scissors\n").lower()
+        valid_choice = is_valid_choice(user)
+
     # This shows the user's choice and the computer's choice
-    print("You: "+user)
-    print("Computer: "+computer)
+    print("You: " + user)
+    print("Computer: " + computer)
 
     if user == computer:
         return '\nIt\'s a tie'
@@ -21,9 +30,22 @@ def play():
     
     return '\nYou lost!'
     
+"""
+This function determines if the player has won based on the three possible win cases.
+It returns true if one of these cases have been achieved.
+"""
 def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+
+"""
+This function returns true if the user inputs 'r', 'p', or 's', 
+and returns false if it is anything else
+"""
+def is_valid_choice(player):
+    if (player == 'r') or (player == 'p') or (player == 's'):
+        return True
+    return False
 
 
 # This variable keeps the game loop going
