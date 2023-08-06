f354e8232e4d85411dc53ee783588e813d88bc58
SierraTran <sierra.tran@mail.com>
2023-08-06 14:18:54 -0400

    Reordered functions, Editted instructions for user

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,7 +6,7 @@ This program simulates the "Rock, Paper, Scissors" game.
 
 def play():
     # The user is prompted for a choice, and the computer randomly chooses.
-    user = input("Pick one... \n'r' for rock, 'p' for paper, 's' for scissors\n").lower()
+    user = input("Pick one... \nType 'r' for rock, 'p' for paper, or 's' for scissors\n").lower()
     computer = random.choice(['r', 'p', 's'])
 
     # This variable uses the is_valid_choice function to make sure the user input can be used for the game
@@ -15,7 +15,7 @@ def play():
     # If the user inut is not valid, this loop will start and run until valid input is received from the user
     while(valid_choice_play == False):
         print(user + " is not a valid choice. Please try again.")
-        user = input("Pick one... \n'r' for rock, 'p' for paper, 's' for scissors\n").lower()
+        user = input("Pick one... \nType 'r' for rock, 'p' for paper, or 's' for scissors\n").lower()
         valid_choice_play = is_valid_choice_play(user)
 
     # This shows the user's choice and the computer's choice
@@ -47,25 +47,24 @@ def is_valid_choice_play(player):
         return True
     return False
 
-def is_valid_choice_player_continue(answer):
-    if (answer == 'y') or (answer == 'n'):
-        return True
-    return False
-
 def player_continue():
-    answer = input("\nWould you like to keep playing? 'y' for yes, 'n' for no\n").lower()
+    answer = input("\nWould you like to keep playing? Type 'y' fr yes or 'n' for no.\n").lower()
 
     valid_choice_continue = is_valid_choice_player_continue(answer)
 
     while(valid_choice_continue == False):
         print("Invalid choice. Please try again.")
-        answer = input("Would you like to keep playing? 'y' for yes, 'n' for no\n").lower()
+        answer = input("Would you like to keep playing? Type 'y' fr yes or 'n' for no.\n").lower()
         valid_choice_continue = is_valid_choice_player_continue(answer)
 
     if answer == 'y':
         return True
     return False
 
+def is_valid_choice_player_continue(answer):
+    if (answer == 'y') or (answer == 'n'):
+        return True
+    return False
 
 # This variable keeps the game loop going
 keep_playing = True
